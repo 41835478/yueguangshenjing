07db0cc408a3e9e1e0ee0fdbeb3fc0d6785ec17a
张振华 <2593481502@qq.com>
2017-10-18 14:36:44 +0800

    no message

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '192.168.10.115',
     // 数据库名
     'database'        => 'api_ygsj',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'api_ygsj',
     // 密码
-    'password'        => '',
+    'password'        => '123456',
     // 端口
     'hostport'        => '',
     // 连接dsn
