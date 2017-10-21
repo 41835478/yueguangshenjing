ea9a4adf34963f8e7614dfc30675d43563dbb210
tianzehao123 <1424622689@qq.com>
2017-10-21 17:15:51 +0800

    清除缓存

---
+++ application/home/controller/Three.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\home\controller;
+
+use think\Controller;
+use think\Request;
+
+class Three extends Controller
+{
+    /**
+     * 三级分佣
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        //
+    }
+}
