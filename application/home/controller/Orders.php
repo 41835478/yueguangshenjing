17b4b76e1f9665c87716be6da337bbd0ac472f75
张振华 <2593481502@qq.com>
2017-10-24 11:35:07 +0800

    no message

--- application/home/controller/Orders.php
+++ application/home/controller/Orders.php
@@ -3,6 +3,8 @@
 namespace app\home\controller;
 
 use think\Controller;
+use think\Db;
+use think\Exception;
 use think\Request;
 use think\Session;
 
@@ -50,7 +52,16 @@ class Orders extends Base
         $id=Session::get('goods_id','home');
         $num=Session::get('num','home');
         if($id&&$num){
-            $this->createOrder($id,$num,$date);
+            if($date['is_shop']==1){
+                if(!$date['shop_name']){
+                    return json(['status'=>true,'message'=>'请填写店铺名称']);
+                }
+            }
+            $res=$this->createOrder($id,$num,$date);
+            if($res){
+                return json(['status'=>true,'message'=>'下单成功']);
+            }
+            return json(['status'=>false,'message'=>'下单失败']);
         }else{
             return json(['status'=>false,'message'=>'请先购买产品']);
         }
@@ -64,11 +75,105 @@ class Orders extends Base
         $date['shop_name']=$data['shop_name'];
         $date['status']=1;
         $date['address_id']=$data['address_id'];
+        $address=$this->getAddress($data['address_id']);
+        $date['phone']=$address->phone;
+        $date['area_info']=$address->area.','.$address->street.','.$address->area_info;
+        $date['price']=$this->getGoods($id)->goods_price;
+        $date['num']=$num;
+        $date['created_at']=time();
+        $res=model('admin/OrderModel')->insertGetId($date);
+        Db::startTrans();
+        try{
+            if($res){
+                $orderData['order_id']=$res;
+                $orderData['goods_id']=$id;
+                $goods=$this->getGoods($id);
+                $orderData['goods_name']=$goods->name;
+                $orderData['goods_pic']=$goods->main_pic;
+                $orderData['goods_price']=$goods->goods_price;
+                $orderData['goods_num']=$num;
+                $result=model('admin/OrderInfo')->data($orderData)->save();
+                if($result){
+                    session('order_id',$res,'home');
+                    Session::delete('goods_id','home');
+                    Session::delete('num','home');
+                    Db::commit();
+                    return true;
+                }else{
+                    throw new Exception();
+                }
+            }else{
+                throw new Exception();
+            }
+        }catch(Exception $e){
+            Db::rollback();
+            return false;
+        }
     }
 
-    public function getAddress($id)//获取地址id
+    public function pay()//跳转支付页面
     {
+        $totalMoney=0;
+        $order_id=Session::get('order_id','home');
+        if($order_id){
+            $order=model('admin/OrderModel')->get($order_id);
+            $totalMoney=$order->price;
+        }
+        $user=model('admin/User')->get($this->user_id);
+        return view('orders/pay',['totalMoney'=>$totalMoney,'user'=>$user]);
+    }
 
+    public function getAddress($id)//获取地址
+    {
+        $address=model('admin/Address')->get($id);
+        return $address;
+    }
+
+    public function getGoods($id)//获取商品
+    {
+        $goods=model('admin/Goods')->get($id);
+        return $goods;
+    }
+
+    public function createPay()//公共的生成支付
+    {
+        $date=input('post.');
+        $order_id=Session::get('order_id','home');
+        if($date['flag']==3){//说明是余额支付
+            if($order_id){
+                $this->balancePay($order_id);
+            }
+            return json(['status'=>false,'message'=>'该订单已支付请不要重复支付']);
+        }
+    }
+
+    public function balancePay($id)//余额支付
+    {
+        Db::startTrans();
+        try{
+            $order=model('admin/OrderModel')->get($id);
+            $order->status=2;
+            if($order->save()){
+
+            }else{
+                throw new Exception();
+            }
+        }catch (Exception $e){
+            Db::rollback();
+            return false;
+        }
+    }
+
+    public function writeRecord()//写入记录
+    {
+        $date['user_id']=$this->user_id;
+        $date['record_info']='购买商品';
+        $date['type']='购买商品';
+    }
+
+    public function paySuccess()//支付成功跳转页面
+    {
+        return view('order/paySuccess');
     }
 
     public function order_num()//生成订单号
