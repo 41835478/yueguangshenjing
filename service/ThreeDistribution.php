6c4f5cc18abec0b41708bceffa18d1748f75fefc
tianzehao123 <1424622689@qq.com>
2017-10-31 12:03:30 +0800

    修复:分佣

--- service/ThreeDistribution.php
+++ service/ThreeDistribution.php
@@ -24,34 +24,40 @@ class ThreeDistribution{
         $order = OrderModel::get($orderid);
         $user = User::all();
 
-        $prent = getUpUser($user,$order->user_id);
+        $prent = getUpUser($user,$order['user_id']);
         $configOne =  Config::get(2)->value / 100;
         $configTwo =  Config::get(3)->value / 100;
         $configThree =  Config::get(4)->value / 100;
 
         $threeArray = ["",$configOne,$configTwo,$configThree];
 
-        $account = new AccountRecord();
-
         if(isset($prent[1]) && $prent[1]){#1
+            Log::record("分销开始!!!");
             Db::startTrans();
             try{
                 for( $i=1; $i<=3; $i++ ){
-                    if(User::where(["id"=>$prent[$i]])->value('status') == 1){#冻结不能享受三级分佣
-                        $resOne = User::where('id',$prent[$i])
-                            ->setInc('account',($order->price* $order->num) * $threeArray[$i]);
+                    if(isset($prent[$i])){
+                        $user = User::where(["id"=>$prent[$i]])->value('status');
+                        if($user == "1"){
+                            if(User::where(["id"=>$prent[$i]])->value('status') == 1){#冻结不能享受三级分佣
+                                $threePrice = ($order['price']* $order['num']) * $threeArray[$i];
+                                $resOne = User::where('id',$prent[$i])
+                                    ->setInc('account',$threePrice);
+                            }
+                            if($resOne){
+                                $account->setAccountRecord($prent[$i],"{$i}级分佣",
+                                    AccountRecordModel::TYPE_ONE,1,
+                                    ($order['price']* $order['num']) * $threeArray[$i],$order['user_id']);
+                                Log::record("{$i}级分佣完成");
+                            }
+                        }
                     }
-                    if($resOne){
-                        $account->setAccountRecord($prent[$i],"{$i}级分佣",
-                            AccountRecordModel::TYPE_ONE,1,
-                            ($order->price* $order->num) * $threeArray[$i],$order->user_id);
-                    }
-                    Log::record("{$i}级分佣完成");
                 }
                 Db::commit();
             }catch (Exception $e){
                 Db::rollback();
             }
+            Log::record("分销结束!!!");
         }
 
     }
