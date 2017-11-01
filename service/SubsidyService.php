c8c52e229f490d3d71f1360bdc77926120273313
tianzehao123 <1424622689@qq.com>
2017-11-01 11:29:55 +0800

    修复:代理商伞下购货补助

--- service/SubsidyService.php
+++ service/SubsidyService.php
@@ -25,10 +25,35 @@ class SubsidyService
         $order = OrderModel::get($orderid);
 
         $account = new AccountRecord();
-        #店面 一级店面  100元补助，直属代理商减100
 
+        if ($order['sign'] == 1 && $order['send_id'] != "") {#代理发货奖 奖励50
+            $user_agent = User::get($order['send_id']);
+            $user_agent->setInc("account", (Config::get(10)->value * $order['num']));
+            $account->setAccountRecord($order['send_id'], "代理商发货奖",
+                12, 2, (Config::get(12)->value * $order['num']),$order['user_id']);
+            Log::record("代理商发货补助完成");
+            file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."代理商发货补助完成\n",
+                FILE_APPEND);
+        }
+        if($order['send_id'] != ""){
+            if ($this->umbrella($order['user_id'], $order['num'],$order['send_id'])) {
+                Log::record("伞下购货补助完成");
+                file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."伞下购货补助完成\n",
+                    FILE_APPEND);
+            }else{
+                file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."上级没有代理商无需补助\n",
+                    FILE_APPEND);
+            }
+
+        }else{
+            file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."上级没有代理商无需补助\n",
+                FILE_APPEND);
+        }
+
+        #店面 一级店面  100元补助，直属代理商减100
         Db::startTrans();
         try {
+
             if ($order->sign == 2) {
                 $user_storefront = User::get($order->shop_id);#查询店面
 
@@ -53,6 +78,9 @@ class SubsidyService
                         1店面补助直属代理扣款\n",FILE_APPEND);
                     }
 
+                }else{
+                    file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."
+                        不是1级店面\n",FILE_APPEND);
                 }
                 if ($user_storefront['level'] == "8") {#二级 补助50 直属代理商余额减去50
                     $user_storefront->setInc("account", (Config::get(12)->value * $order['num']));
@@ -73,26 +101,16 @@ class SubsidyService
                         file_put_contents("./log.txt",date("Y-m-d H:i:s",time()).
                             "2店面补助直属代理扣款\n",FILE_APPEND);
                     }
+                }else{
+                    file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."
+                        不是2级店面\n",FILE_APPEND);
                 }
 
+            }else{
+                file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."
+                        不是店面\n",FILE_APPEND);
             }
             #代理商
-            if ($order['sign'] == 1 && $order['send_id'] != "") {#代理发货奖 奖励50
-                $user_agent = User::get($order['send_id']);
-                $user_agent->setInc("account", (Config::get(10)->value * $order['num']));
-                $account->setAccountRecord($order->send_id, "代理商发货奖",
-                    12, 2, (Config::get(10)->value * $order['num']),$order['user_id']);
-                Log::record("代理商发货补助完成");
-                file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."代理商发货补助完成\n",
-                    FILE_APPEND);
-            }
-
-            if ($this->umbrella($order['user_id'], $order['num'])) {
-                Log::record("伞下购货补助完成");
-                file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."伞下购货补助完成\n",
-                    FILE_APPEND);
-            }
-
 //            if ($order['sign'] == 3) {
 //                Log::record("平台发货!无需补助");
 //                file_put_contents("./log.txt",date("Y-m-d H:i:s",time())."平台发货!无需补助\n",
@@ -103,10 +121,17 @@ class SubsidyService
                 $user_agent = User::get($order['send_id']);
                 $user_agent->setInc("account", Config::get(14)->value * $order['num']);
                 $account->setAccountRecord($order['send_id'], "招商销售奖",
-                    3, 2, Config::get(10)->value * $order['num'],$order['user_id']);
+                    3, 2, Config::get(14)->value * $order['num'],$order['user_id']);
                 Log::record("升级后的代理商每销售1台 奖励60");
+                if($account){
+                    file_put_contents("./log.txt",date("Y-m-d H:i:s",time()).
+                        "奖励成 奖励60\n",FILE_APPEND);
+                }else{
+                    file_put_contents("./log.txt",date("Y-m-d H:i:s",time()).
+                        "奖励no 奖励60\n",FILE_APPEND);
+                }
                 file_put_contents("./log.txt",date("Y-m-d H:i:s",time()).
-                    "升级后的代理商每销售1台 奖励60",FILE_APPEND);
+                    "升级后的代理商每销售1台 奖励60\n",FILE_APPEND);
             }
 
             file_put_contents("./log.txt",date("Y-m-d H:i:s",time()).
@@ -119,15 +144,15 @@ class SubsidyService
     }
 
     #理商伞下购货，额外补助200元/台（同级别不重复拿）
-    public function umbrella($id, $num)
+    public function umbrella($id, $num,$sendid)
     {
-        $user_list = getUpUser(User::all(), $id);
+        $user_list = getUpUser(User::all(), $sendid);
         $account = new AccountRecord();
         for ($i = 0; $i < count($user_list); $i++) {
             if (in_array(User::get($user_list[$i])['level'], [3, 4, 5, 6])) {
                 User::get($user_list[$i])->setInc("account", (Config::get(9)->value * $num));
-                $account->setAccountRecord($id, "代理商伞下购货",
-                    11, 2, (Config::get(9)->value * $num - Config::get(12)->value * $num));
+                $account->setAccountRecord($sendid, "代理商伞下购货",
+                    11, 2, (Config::get(9)->value * $num - Config::get(12)->value * $num),$id);
                 if ($account) {
                     return true;
                 }
